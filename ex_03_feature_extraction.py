9da8c26f9b92170ca36fa8863e0ba43040f6f44e
Jan Alister Nolte <1849420@uni-wuppertal.de>
2025-05-25 11:17:29 +0200

    implemented kmeans algorithm

--- ex_03_feature_extraction.py
+++ ex_03_feature_extraction.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from scipy.signal import detrend, windows
+from scipy.signal import detrend, windows, find_peaks
 
 
 def find_dominant_frequencies(x: np.ndarray, fs: int) -> np.ndarray:
@@ -14,7 +14,43 @@ def find_dominant_frequencies(x: np.ndarray, fs: int) -> np.ndarray:
     Returns:
         np.ndarray: The dominant frequencies for each signal, shape: (num_samples,).
     """
-    pass
+    # initialize empty array with shape matching the first dimension of the input array x
+    dominant_frequencies = np.zeros(x.shape[0])
+
+    # loop through each signal to find the dominant frequency
+    for i, signal in enumerate(x):
+        # Detrending
+        #   remove linear trends affecting all data points -> center signal around mean
+        detrended_signal = detrend(signal)
+
+        # Windowing
+        #   using Hann windowing as nature/shape of signal is unknown/likely combination of sines (?)
+        #   apply window by multiplying element-wise with the detrended signal
+        window = windows.hann(len(detrended_signal))
+        windowed_signal = detrended_signal * window
+
+        # Compute fast fourier transform (FFT)
+        #   converts signal from time-domain to frequence-domain
+        #   get magnitude to simplify values from complex to real and discard mirrored negative values
+        fft_result = np.fft.fft(windowed_signal)
+        fft_magnitude = np.abs(fft_result)
+        fft_magnitude = fft_magnitude[:len(fft_magnitude)//2]
+
+        # Compute PSD
+        #   PSD helps in separating dominant frequencies from noise
+        #   take the squared absolute fft values to the power of 2 and divide by sampling frequency * sequence length
+        psd = (fft_magnitude ** 2) / (fs * len(windowed_signal))
+
+        # Frequency axis
+        #   create Hz axis of frequencies aligned with PSD result axis
+        frequencies = np.fft.fftfreq(len(windowed_signal), 1/fs)[:len(windowed_signal)//2]
+
+        # Find the dominant frequency
+        #   get maximum value in calculated PSD and store it in appropriate place in return array
+        dominant_freq = frequencies[np.argmax(psd)]
+        dominant_frequencies[i] = dominant_freq
+
+    return dominant_frequencies
 
 
 def extract_features(data: np.ndarray, labels: np.ndarray) -> pd.DataFrame:
@@ -26,5 +62,64 @@ def extract_features(data: np.ndarray, labels: np.ndarray) -> pd.DataFrame:
     Returns:
         pd.DataFrame: The extracted features.
     """
-    pass
+    fs = 100_000
+
+    # Separate voltage and current signals
+    currents = data[:, :, 0]
+    voltages = data[:, :, 1]
+
+    # Frequency-domain features using existing function
+    curr_dom_freqs = find_dominant_frequencies(currents, fs)
+    volt_dom_freqs = find_dominant_frequencies(voltages, fs)
+
+    # setup empty array to fill in for each given sample
+    features = []
+
+    for i in range(data.shape[0]):
+        volt = voltages[i]
+        curr = currents[i]
+        power = volt * curr
+
+        volt_peaks, _ = find_peaks(volt)
+        curr_peaks, _ = find_peaks(curr)
+
+        volt_time_to_peak = np.argmax(volt) / fs
+        curr_time_to_peak = np.argmax(curr) / fs
+
+        feat = {
+            # Voltage features
+            'volt_mean': np.mean(volt),
+            'volt_std': np.std(volt),
+            'volt_min': np.min(volt),
+            'volt_max': np.max(volt),
+            'volt_median' : np.median(volt),
+            'volt_num_peaks': len(volt_peaks),
+            'volt_time_to_peak': volt_time_to_peak,
+
+            # Current features
+            'curr_mean': np.mean(curr),
+            'curr_std': np.std(curr),
+            'curr_min': np.min(curr),
+            'curr_max': np.max(curr),
+            'curr_median': np.median(curr),
+            'curr_num_peaks': len(curr_peaks),
+            'curr_time_to_peak': curr_time_to_peak,
+
+            # Power features
+            'power_mean': np.mean(power),
+            'power_std': np.max(power),
+            'power_min': np.min(power),
+            'power_max': np.max(power),
+
+            # Frequency domain
+            'volt_dom_freq': volt_dom_freqs[i],
+            'curr_dom_freq': curr_dom_freqs[i],
+        }
+
+        features.append(feat)
+
+    df = pd.DataFrame(features)
+    df['labels'] = labels
+
+    return df
 
--- ex_04_my_kmeans.py
+++ ex_04_my_kmeans.py
@@ -45,7 +45,59 @@ class MyKMeans:
             raise ValueError("Input data must be a numpy array or a pandas DataFrame")
         
         # Code here
+        dim = len(x.shape)
+
+        if dim < 2 or dim > 3:
+            raise ValueError("Input data must be a 2D or 3D array")
+
+        # select initial centroids, either random or kmeans++
+        if self.init_method == "kmeans++":
+            self.centroids = self._initialize_centroids(x)
+        elif self.init_method == "random":
+            indices = np.random.sample(range(x.shape[0]), self.k)
+            if dim == 2:
+                self.centroids = x[indices, :]
+            else:
+                self.centroids = x[indices, :, :]
+        else:
+            raise ValueError("Initialization method must be 'kmeans++' or 'random'")
+
+        # setup empty clusters
+        clusters = np.empty(shape=(x.shape[0]))
+        clusters[:] = np.nan
         
+        with tqdm(range(self.max_iter), desc="KMeans", unit="iter") as pbar:
+            for _ in pbar:
+                # get distances
+                distances = self._compute_distance(x, self.centroids)
+
+                # assign each sample to cluster of lowest distance
+                new_clusters = np.empty(shape=(x.shape[0]))
+                new_clusters[:] = np.nan
+
+                for i in range(distances.shape[0]):
+                    c = np.argmin(distances[i])
+                    new_clusters[i] = c
+
+                if np.array_equal(clusters, new_clusters):
+                    # no more changes detected -> break out of for loop
+                    break
+                
+                # calculate new centroids as means of all points within cluster
+                clusters = np.copy(new_clusters)
+                for c in range(self.k):
+                    points_in_cluster = x[np.where(clusters == c)]
+                    # assure cluster is not empty before calculating new centroid
+                    if points_in_cluster.shape[0] > 0:
+                        self.centroids[c] = np.mean(points_in_cluster, axis=0)
+                    else:
+                        # empty cluster encountered: select a random point as centroid for this emtpy cluster
+                        self.centroids[c] = x[np.random.randint(0, x.shape[0])]
+                
+                # update progress bar with current iteration inertia (i.e. fit quality)
+                self.inertia_ = self._compute_inertia(x, self.centroids)
+                pbar.set_postfix({"inertia": self.inertia_})
+
         return self
     
 
@@ -88,7 +140,31 @@ class MyKMeans:
         Returns:
             np.ndarray: Initial centroids.
         """
-        pass
+        dim = len(x.shape)
+
+        if dim == 2:
+            centroids = np.empty(shape=(self.k, x.shape[1]))
+        else:
+            centroids = np.empty(shape=(self.k, x.shape[1], x.shape[2]))
+        
+        centroids[:] = np.nan
+
+        # choose first centroid randomly
+        idx = np.random.randint(x.shape[0])
+        if dim == 2:
+            centroids[0] = x[idx, :]
+        else:
+            centroids[0] = x[idx, :, :]
+        
+        for c in range(self.k - 1):
+            distances = self._compute_distance(x, centroids)
+            # select next point as centroid with max distance to previous centroids
+            if dim == 2:
+                centroids[c+1] = x[np.argmax(distances), :]
+            else:
+                centroids[c+1] = x[np.argmax(distances), :, :]
+        
+        return np.array(centroids)
 
     def _compute_distance(self, x: np.ndarray, centroids: np.ndarray) -> np.ndarray:
         """
@@ -101,8 +177,45 @@ class MyKMeans:
         Returns:
             np.ndarray: Distances between each sample and each centroid, shape (n_samples, k).
         """
-        pass
+        if self.distance_metric == "dtw":
+            return self._dtw(x, centroids)
+        elif self.distance_metric != "euclidean" and self.distance_metric != "manhattan":
+            raise ValueError("Invalid distance metric")
 
+        distances = np.empty(shape=(x.shape[0], centroids.shape[0]))
+        distances[:] = np.nan
+
+        dim = len(x.shape)
+
+        if dim == 2:
+            for i in range(x.shape[0]):
+                for k in range(centroids.shape[0]):
+                    sum = 0
+                    for j in range(x.shape[1]):
+                        dist = x[i, j] - centroids[k, j]
+                        if self.distance_metric == "euclidean":
+                            sum += (dist)**2
+                        elif self.distance_metric == "manhattan":
+                            sum += abs(dist)
+                    if self.distance_metric == "euclidean":
+                        sum = np.sqrt(sum)
+                    distances[i, k] = sum
+        else:
+            for i in range(x.shape[0]):
+                for k in range(centroids.shape[0]):
+                    sum = 0
+                    for t in range(x.shape[1]):
+                        for j in range(x.shape[2]):
+                            dist = x[i, t, j] - centroids[k, t, j]
+                            if self.distance_metric == "euclidean":
+                                sum += (dist)**2
+                            elif self.distance_metric == "manhattan":
+                                sum += abs(dist)
+                        if self.distance_metric == "euclidean":
+                            sum = np.sqrt(sum)
+                        distances[i, k] = sum
+
+        return np.array(distances)
 
     def _dtw(self, x: np.ndarray, centroids: np.ndarray) -> np.ndarray:
         """
@@ -115,4 +228,28 @@ class MyKMeans:
         Returns:
             np.ndarray: DTW distances between each sample and each centroid, shape (n_samples, k).
         """
-        pass
+        distances = np.empty((x.shape[0], centroids.shape[0]))
+        distances[:] = np.nan
+
+        dim = len(x.shape)
+
+        if dim == 2:
+            for i in range(x.shape[0]):
+                for k in range(centroids.shape[0]):
+                    distances[i, k] = dtw.distance_fast(x[i], centroids[k], only_ub=False)
+        else:
+            for i in range(x.shape[0]):
+                for k in range(centroids.shape[0]):
+                    for j in range(x.shape[2]):
+                        distances[i, k] += dtw.distance_fast(x[i, :, j], centroids[k, :, j], only_ub=False)
+
+        return distances
+
+
+    def _compute_inertia(self, x: np.ndarray, centroids: np.ndarray) -> float:
+        inertia = 0.0
+        distances = self._compute_distance(x, centroids)
+        for i in range(distances.shape[0]):
+            inertia += np.argmin(distances[i])
+        
+        return inertia
